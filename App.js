e0b7f9a969d5586b643c662ec702490846f15f20
Gabriel Kuzava <101483110+kuzag015@users.noreply.github.com>
2022-07-10 16:32:04 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Dojo!</h1>
+      <h2>Things I need to do:</h2>
+      <ul>
+        <li>Make dinner</li>
+        <li>Feed my cat</li>
+        <li>Go to sleep</li>
+        <li>Sleep forever</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
